d845ed94548cdec53a27cedc89f9163c630f6285
vrklkv <101700733+vrklkv@users.noreply.github.com>
2024-12-16 20:52:43 +0300

    Update simulation.cpp

--- simulation.cpp
+++ simulation.cpp
@@ -5,20 +5,22 @@
 #include "parcing/py_runner.h"
 
 int main() {
-    //запускаем парсер на питоне
-    std::string py_script_path = "parcing/parcer.py"; 
+    //Запускаем парсер на питоне
+    //std::string py_script_path = "parcing/parcer.py"; //Работает на MacOS
+    std::string py_script_path = "../parcing/parcer.py"; //Работает на Windows
     int result = run_python_script(py_script_path);
 
-    ifstream inputFile("parcing/data.txt");//Открываем файл
+    //ifstream inputFile("parcing/data.txt"); //Работает на MacOS
+    ifstream inputFile("../parcing/data.txt");//Работает на Windows
     if (!inputFile.is_open()) {
         cerr << "Error: Unable to open data.txt" << endl;
         return 1;
     }
-    map <string, vector <double> > historical_rates;//Создаем словарь
+    map <string, vector <double> > historical_rates; //Создаем словарь
     historical_rates["USD"] = vector<double>();
     historical_rates["EUR"] = vector<double>();
 
-    string line;//Читаем данные из файла
+    string line;//Читаем данные из файла, созданного парсером
     while (getline(inputFile, line)) {
         stringstream ss(line);
         float usd_rate, eur_rate;
@@ -26,12 +28,12 @@ int main() {
             historical_rates["USD"].push_back(usd_rate);
             historical_rates["EUR"].push_back(eur_rate);
         } else {
-            cerr << "Warning: Invalid data in line: " << line << ". Skipping." << endl;//Проверка корректного чтения
+            cerr << "Warning: Invalid data in line: " << line << ". Skipping." << endl;
         }
     }
     inputFile.close();
 
-    int days = 180; //approximately half a year
+    int days = 180;
     int simulations = 100000;
     map<string, double> drifts;
     map<string, double> volatilities;
@@ -44,14 +46,11 @@ int main() {
 
     map<string, vector<vector<double> > > simulated_rates = simulate_exchange_rates(historical_rates, days, simulations, drifts, volatilities);
     /*  //To see the simulated rates
-	for (const auto& pair : simulated_rates)
-    {
+	for (const auto& pair : simulated_rates) {
       cout << "Currency: " << pair.first << endl;
-      for(int i = 0; i < days; i++)
-      {
+      for(int i = 0; i < days; i++) {
           cout << "Day " << i + 1 << ": ";
-           for(int j = 0; j < simulations; j++)
-           {
+           for(int j = 0; j < simulations; j++) {
               cout << fixed << setprecision(3) << pair.second[i][j] << " ";
            }
         cout << endl;
