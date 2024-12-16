9c2c09bb926580e6ef5ecab23a3aaaf3c0c7ec87
vrklkv <101700733+vrklkv@users.noreply.github.com>
2024-12-16 23:35:34 +0300

    Add files via upload

---
+++ prasing/parser.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import yfinance as yf 
+import os
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+s = datetime.now() - relativedelta(years=10)
+e = datetime.now()
+data = yf.download("USDRUB=X EURRUB=X", start=s, end=e)
+usd_rates = data['Close']['USDRUB=X'].dropna()
+eur_rates = data['Close']['EURRUB=X'].dropna()
+frames = [usd_rates, eur_rates]
+result = pd.concat(frames, axis=1)
+#result.to_csv('result1.csv', index=True) 
+current_file = os.path.realpath(__file__)
+current_directory = os.path.dirname(current_file)
+#excel_writer=str(current_directory)+"\data.xlsx"
+where=str(current_directory)+"/data.txt"
+#result.to_excel(excel_writer, sheet_name='Sheet1', index=True)
+np.savetxt(where, result.values, fmt='%f')
